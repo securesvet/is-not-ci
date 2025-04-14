c7879b2279aa6078d83d8cee5be5977d92df4164
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-15 03:46:57 +0500

    fix: modified test

--- test.js
+++ test.js
@@ -10,6 +10,9 @@ assert(!isNotCI)
 
 delete process.env.CI
 delete process.env.CONTINUOUS_INTEGRATION
+delete process.env.BUILD_NUMBER
+delete process.env.TRAVIS
+delete process.env.GITHUB_ACTIONS
 
 clearModule('./')
 clearModule('ci-info')
